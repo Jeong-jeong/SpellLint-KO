0a5584e9cb75a98188717ada9fd0632cd247813d
(FE) Noopy_jeongs <kimm.jeongs@gmail.com>
2024-10-19 21:30:16 +0900

    feat: initialize buffer

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,63 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { loadModule, HunspellFactory, Hunspell } from 'hunspell-asm';
+import * as fs from 'fs';
+import * as path from 'path';
+import checkSpelling from './utils/checkSpelling';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
+export async function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "spell-lint-ko" is now active!');
+	// DiagnosticCollection 생성
+	// 이 컬렉션이 모든 맞춤법 오류를 저장하고 관리.
+	const diagnosticCollection = vscode.languages.createDiagnosticCollection('spell-lint-ko');
+	// 확장 프로그램이 비활성화될 때 자동으로 정리되도록 context.subscriptions에 추가
+	context.subscriptions.push(diagnosticCollection);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('spell-lint-ko.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from SpellLint-KO!');
-	});
+	try {
+		const dictionaryPath = path.join(context.extensionPath, 'dictionaries');
+		const affPath = path.join(dictionaryPath, 'ko.aff');
+		const dicPath = path.join(dictionaryPath, 'ko.dic');
+		const affBuffer = await fs.promises.readFile(affPath);
+		const dicBuffer = await fs.promises.readFile(dicPath);
+		const affUint8Array = new Uint8Array(affBuffer);
+		const dicUint8Array = new Uint8Array(dicBuffer);
+		
+		const hunspellFactory = await loadModule();
+		const affFile = hunspellFactory.mountBuffer(affUint8Array);
+		const dicFile = hunspellFactory.mountBuffer(dicUint8Array);
+		const hunspell = hunspellFactory.create(affFile, dicFile);
 
-	context.subscriptions.push(disposable);
+		const disposable = vscode.commands.registerCommand('spellLintKo.checkSpelling', () => {
+			// The code you place here will be executed every time your command is executed
+			// Display a message box to the user
+			vscode.window.showInformationMessage('Hello World from SpellLint-KO!');
+		});
+	
+		context.subscriptions.push(disposable);
+	
+		context.subscriptions.push(
+			vscode.workspace.onDidOpenTextDocument((document) => {
+				checkSpelling({ document, hunspell, diagnosticCollection });
+			})
+		);
+		context.subscriptions.push(
+			vscode.workspace.onDidChangeTextDocument((event) => {
+				checkSpelling({ document: event.document, hunspell, diagnosticCollection });
+			})
+		);
+		
+
+	} catch (error) {
+		console.log(error, 'error');
+	}
+
+	
+	
+	// checkSpelling({
+	// 	document: 
+	// })
+	// const dictionaryPath = path.join(context.extensionPath, 'dictionaries');
+	// const affPath = path.join(dictionaryPath, 'ko.aff');
+	// const dicPath = path.join(dictionaryPath, 'ko.dic');
 }
 
 // This method is called when your extension is deactivated
